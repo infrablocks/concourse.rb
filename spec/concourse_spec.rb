2fd57d115ce91f16db40d68fcc4e0cdeceee64d0
Toby <tobyclemson@gmail.com>
2022-03-11 20:43:51 +0000

    Bump Ruby to 2.7.5. Require 2.7 or later in gemspec.

--- spec/concourse_spec.rb
+++ spec/concourse_spec.rb
@@ -4,6 +4,6 @@ require 'spec_helper'
 
 RSpec.describe Concourse do
   it 'has a version number' do
-    expect(Concourse::VERSION).not_to be nil
+    expect(Concourse::VERSION).not_to be_nil
   end
 end
